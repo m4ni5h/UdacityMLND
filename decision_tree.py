df6aecd57dd3f9ad753b6931d8ac2ee31a5d16fd
deeplearning <manish.kumar5139@gmail.com>
2019-04-22 22:08:39 +0530

    sample decision tree

---
+++ decision_tree.py
@@ -0,0 +1,32 @@
+# Import statements 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+
+# Read the data.
+data = np.asarray(pd.read_csv('data.csv', header=None))
+# Assign the features to the variable X, and the labels to the variable y. 
+''' data sample
+0.24539,0.81725,0
+0.21774,0.76462,0
+0.20161,0.69737,0
+0.20161,0.58041,0
+'''
+X = data[:,0:2]
+y = data[:,2]
+
+# TODO: Create the decision tree model and assign it to the variable model.
+# You won't need to, but if you'd like, play with hyperparameters such
+# as max_depth and min_samples_leaf and see what they do to the decision
+# boundary.
+model = DecisionTreeClassifier()
+
+# TODO: Fit the model.
+model.fit(X,y)
+
+# TODO: Make predictions. Store them in the variable y_pred.
+y_pred = model.predict(X)
+
+# TODO: Calculate the accuracy and assign it to the variable acc.
+acc = accuracy_score(y, y_pred)
