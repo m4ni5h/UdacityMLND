ed8114f1c9d818b8e7ab00b21a9c9df3cf69f41c
Kumar <manish.kumar5139@gmail.com>
2019-10-16 10:54:20 +0530

    Printing the perceptron output

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -6,6 +6,18 @@ import numpy as np
 # Setting the random seed, feel free to change it and see different solutions.
 np.random.seed(42)
 
+def plot_points(X, y):
+    admitted = X[np.argwhere(y==1)]
+    rejected = X[np.argwhere(y==0)]
+    plt.scatter([s[0][0] for s in rejected], [s[0][1] for s in rejected], s = 25, color = 'blue', edgecolor = 'k')
+    plt.scatter([s[0][0] for s in admitted], [s[0][1] for s in admitted], s = 25, color = 'red', edgecolor = 'k')
+
+def display(m, b, color='g--'):
+    plt.xlim(-0.05,1.05)
+    plt.ylim(-0.05,1.05)
+    x = np.arange(-10, 10, 0.1)
+    plt.plot(x, m*x+b, color)
+
 def stepFunction(t):
     if t >= 0:
         return 1
@@ -48,12 +60,13 @@ def trainPerceptronAlgorithm(X, y, learn_rate = 0.01, num_epochs = 25):
         # In each epoch, we apply the perceptron step.
         W, b = perceptronStep(X, y, W, b, learn_rate)
         boundary_lines.append((-W[0]/W[1], -b/W[1]))
+        display(-W[0]/W[1], -b/W[1])
     return boundary_lines
 
-plotdata = pd.read_csv('data.csv') 
-
-print(plotdata)
-plotdata.plot()
-plt.plot([plotdata.iloc[:,0]], [plotdata.iloc[:,1]], 'ro')
-plt.axis([0, 6, 0, 20])
+data = pd.read_csv('data.csv', header=None)
+X = np.array(data[[0,1]])
+y = np.array(data[2])
+plot_points(X,y)
+linear_line = trainPerceptronAlgorithm(X, y)
+display(linear_line[-1][0], linear_line[-1][1], 'black')
 plt.show()
